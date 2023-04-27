bbf795161941473cf3e36d0dfe41eae340cd82ee
Nerdy Birdy <holliesundra@gmail.com>
2023-04-26 21:47:40 -0700

    changed some colors and text and thought button

--- client/src/App.js
+++ client/src/App.js
@@ -46,11 +46,13 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-      
+
+
       <div class="background-container">
         <div class="stars"></div>
         <div class="twinkling"></div>
         </div>
+
         <div className="flex-column justify-flex-start min-100-vh">
           <Header />
           <div className="container">
