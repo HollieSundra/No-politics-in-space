76a60295d12ffe98c40fe62b9e5caff99bae57ad
James Sundra <pojo226@gmail.com>
2023-04-30 20:33:50 -0700

    ewok love

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -43,6 +43,8 @@ const Login = (props) => {
   return (
     <main className="flex-row justify-center mb-4">
       <div className="col-12 col-lg-10">
+        <img src={ewokcute} alt="" className="ewok" />
+        <img src={ewokcute1} alt="" className="ewok1" />
         <div className="card">
           <h4 className="card-header bg-dark text-light p-2">Login</h4>
           <div className="card-body">
